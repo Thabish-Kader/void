97a3b478121c1d228c218121d9270e93e4820dc3
Thabish <kadertabish2@gmail.com>
2025-03-30 14:08:15 +0400

    refactored code
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ frontend/api/index.ts
@@ -0,0 +1,34 @@
+import { axiosInstance } from "@/utils";
+
+export const handleFilesUpload = async (
+  files: File[],
+  email: string,
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  if (!files) return;
+  setIsLoading(true);
+  try {
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append("files", files[i]);
+    }
+    formData.append("storageClass", "STANDARD");
+    formData.append("email", email);
+
+    const res = await axiosInstance.post(
+      "/upload/upload-files/1234sdfsdf99",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setIsLoading(false);
+  }
+};
